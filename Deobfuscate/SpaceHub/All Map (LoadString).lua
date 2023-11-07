8f72a5a3026614b57e71e2d3f9ae3509d76725ae
SixZens <106856251+SixZensED@users.noreply.github.com>
2023-11-07 09:37:59 +0700

    Create All Map (LoadString).lua

---
+++ Deobfuscate/SpaceHub/All Map (LoadString).lua
@@ -0,0 +1,147 @@
+function __secureeq(_ARG_0_, _ARG_1_)
+  _ARG_0_ = 0
+  _ARG_0_, _ARG_0_ = 0, _ARG_0_
+  _ARG_0_ = _ARG_1_
+  pcall(function()
+    _UPVALUE0_ = #_UPVALUE1_
+    _UPVALUE2_ = #_UPVALUE3_
+  end)
+  if _ARG_0_ ~= _ARG_1_ then
+  elseif _ARG_0_ ~= _ARG_1_ then
+  else
+  end
+  return _ARG_1_ == _ARG_0_ and 0 - 0 == 0 - 0 and 0 - 0 == 0 - 0
+end
+OxLnIEfOWzYI = {}
+if _ ~= "discord.gg/gQEH2uZxUk" then
+  return
+end
+function HqW_qZhx(_ARG_0_, _ARG_1_, _ARG_2_)
+  if not OxLnIEfOWzYI[_ARG_1_] then
+  else
+    return OxLnIEfOWzYI[_ARG_1_]
+  end
+  for _FORV_8_ = 1, #_ARG_0_ do
+  end
+  _FOR_[_ARG_1_] = "" .. string.char((_ARG_0_:sub(_FORV_8_, _FORV_8_):byte() + 1) % 256)
+  return "" .. string.char((_ARG_0_:sub(_FORV_8_, _FORV_8_):byte() + 1) % 256)
+end
+if game.PlaceId ~= 155615604 then
+else
+  loadstring(game:HttpGet("https://pastebin.com/raw/Sc3Pdarc"))()
+end
+if game.PlaceId == 7167319176 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasfin000/SpaceHub/main/Chicago%20Remastered.lua"))()
+end
+if game.PlaceId ~= 9281034297 then
+else
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasfin000/SpaceHub/main/GoalKickSimulator.lua"))()
+end
+if game.PlaceId == 9281034297 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasfin000/SpaceHub/main/Raiseafloppa.lua"))()
+end
+if game.PlaceId ~= 6884319169 then
+else
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasfin000/SpaceHub/main/micup.lua"))()
+end
+if game.PlaceId ~= 5490351219 then
+else
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasfin000/SpaceHub/main/Clicker%20Madness.lua"))()
+end
+if game.PlaceId ~= 2906554815 then
+else
+  game.ReplicatedStorage.addXP:FireServer(9999999)
+end
+if game.PlaceId ~= 2445600238 then
+else
+  game.ReplicatedStorage.addXP:FireServer(9999999)
+end
+if game.PlaceId ~= 286090429 then
+else
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasfin000/SpaceHub/main/Protected%20(5).lua"))()
+end
+if game.PlaceId == 7444263453 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Nameless9000/RbxScripts/main/JetpackJumpersInfMoney.lua"))()
+end
+if game.PlaceId == 2577423786 then
+  loadstring(game:HttpGet("https://pastebin.com/raw/ki1dmHd8", true))()
+end
+if game.PlaceId ~= 1997193809 then
+else
+  _G.auto = true
+  while _G.auto == true do
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/DeathAsgel/a/main/z", true))()
+    wait(1)
+  end
+end
+if game.PlaceId == 3851622790 then
+  game:GetService("ReplicatedStorage").RemoteEvents.OutsideRole:FireServer("SwatGun", true)
+end
+if game.PlaceId == 2577423786 then
+  loadstring(game:HttpGet("https://pastebin.com/raw/ki1dmHd8", true))()
+end
+if game.PlaceId == 4913581664 then
+  game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(309.7381591796875, 852.6570434570312, 321.1290588378906)
+end
+if game.PlaceId == 5326405001 then
+  loadstring(game:HttpGet("https://pastebin.com/raw/RacPwwzq"))()
+end
+if game.PlaceId ~= 4661110989 then
+else
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Blue-v3rm/blue-s-stuff/master/bigfoot.lua"))()
+end
+if game.PlaceId == 21532277 then
+  loadstring(game:GetObjects("rbxassetid://4763830754")[1].Source)()
+end
+if game.PlaceId ~= 8270679637 then
+else
+  game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(63.41770935058594, 1696.5533447265625, -854.55126953125)
+end
+if game.PlaceId == 3956818381 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasfin000/SpaceHub/main/SpaceHub.lua"))()
+end
+if game.PlaceId == 8750997647 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasfin000/SpaceHub/main/SpaceHub.lua"))()
+end
+if game.PlaceId == 9300344892 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasfin000/SpaceHub/main/SpaceHub.lua"))()
+end
+if game.PlaceId ~= 1224212277 then
+else
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasfin000/SpaceHub/main/SpaceHub.lua"))()
+end
+if game.PlaceId ~= 3527629287 then
+else
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasfin000/SpaceHub/main/SpaceHub.lua"))()
+end
+if game.PlaceId ~= 6677985923 then
+else
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasfin000/SpaceHub/main/SpaceHub.lua"))()
+end
+if game.PlaceId ~= 3823781113 then
+else
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasfin000/SpaceHub/main/SpaceHub.lua"))()
+end
+if game.PlaceId ~= 3102144307 then
+else
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasfin000/SpaceHub/main/SpaceHub.lua"))()
+end
+if game.PlaceId ~= 4924922222 then
+else
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasfin000/SpaceHub/main/Protected%20(7).lua"))()
+end
+if game.PlaceId ~= 3233893879 then
+else
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasfin000/SpaceHub/main/Bad%20Business%203.07%20SILENT%20AIM%20-%20OPEN%20SOURCE%20-%20JUNE%202022%20FREE%20SCRIPT.txt"))()
+end
+if game.PlaceId ~= 5499911356 then
+else
+  game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-356.79083251953125, 701.0478515625, -2131.89111328125)
+end
+if game.PlaceId ~= 5959668085 then
+else
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasfin000/SpaceHub/main/killallcarbonengine"))()
+end
+if game.PlaceId == 4991214437 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasfin000/SpaceHub/main/Town"))()
+end

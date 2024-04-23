0024a8672d47b1ea627887786ba7cc145b16ca20
SixZens <106856251+SixZensED@users.noreply.github.com>
2024-04-23 23:47:14 +0700

    Update update.lua

--- Addons/Library/update.lua
+++ Addons/Library/update.lua
@@ -1,33 +1,8 @@
 local gamelist = {
-  [10277607801] = {
-    ["Pack"] = "Horror Pack",
-    ["Last Update"] = "23/4/2024",
-    ["Status"] = "Working",
-  },
-  [5275822877] = {
-    ["Pack"] = "Normal Pack",
-    ["Last Update"] = "23/4/2024",
-    ["Status"] = "Risk",
-  },
-  [13772394625] = {
+  [5650396773] = {
     ["Pack"] = "Normal Pack",
     ["Last Update"] = "23/4/2024",
     ["Status"] = "Working",
-  },
-  [1730877806] = {
-    ["Pack"] = "Simulator Pack",
-    ["Last Update"] = "23/4/2024",
-    ["Status"] = "Banned",
-  },
-  [4480809144] = {
-    ["Pack"] = "Simulator Pack",
-    ["Last Update"] = "23/4/2024",
-    ["Status"] = "Banned",
-  },
-  [5991163185] = {
-    ["Pack"] = "Simulator Pack",
-    ["Last Update"] = "23/4/2024",
-    ["Status"] = "Banned",
   },
 }
 

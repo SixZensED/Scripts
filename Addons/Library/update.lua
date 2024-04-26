c60dcde5bb392faf69cdefec58b595188da56aba
SixZens <106856251+SixZensED@users.noreply.github.com>
2024-04-26 14:08:23 +0700

    Update update.lua

--- Addons/Library/update.lua
+++ Addons/Library/update.lua
@@ -1,7 +1,12 @@
 local gamelist = {
   [5650396773] = {
     ["Pack"] = "Normal Pack",
-    ["Last Update"] = "23/4/2024",
+    ["Last Update"] = "26/4/2024",
+    ["Status"] = "Working",
+  },
+  [1202096104] = {
+    ["Pack"] = "Kaitun Pack",
+    ["Last Update"] = "26/4/2024",
     ["Status"] = "Working",
   },
 }
